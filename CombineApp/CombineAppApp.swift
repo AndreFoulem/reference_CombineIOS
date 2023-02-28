3b2842e868204f5f3110226022c415be58c6a296
AndreFoulem <timmycasual@icloud.com>
2023-02-27 19:26:42 -0800

    Initial Commit

---
+++ CombineApp/CombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAppApp.swift
+//  CombineApp
+//
+//  Created by AndreMacBook on 2023-02-27.
+//
+
+import SwiftUI
+
+@main
+struct CombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
