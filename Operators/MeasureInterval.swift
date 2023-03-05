f7daf0ed5aa631c84573dfcba21f2715d230abe1
AndreFoulem <timmycasual@icloud.com>
2023-03-04 17:08:41 -0800

    Add Measure Interval Op

---
+++ Operators/MeasureInterval.swift
@@ -0,0 +1,20 @@
+//
+//  MeasureInterval.swift
+//  CombineApp
+//
+//  Created by AndreMacBook on 2023-03-04.
+//
+
+import SwiftUI
+
+struct MeasureInterval: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeasureInterval_Previews: PreviewProvider {
+    static var previews: some View {
+        MeasureInterval()
+    }
+}
