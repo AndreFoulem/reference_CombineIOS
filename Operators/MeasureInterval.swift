7553a4ba147aabfde5ed18e3bac8d55e531a2fff
AndreFoulem <timmycasual@icloud.com>
2023-03-04 17:18:11 -0800

    Add model init()

--- Operators/MeasureInterval.swift
+++ Operators/MeasureInterval.swift
@@ -7,12 +7,23 @@
 
 import SwiftUI
 import Combine
+import Foundation
 
 class MeasureInterval_Model: ObservableObject {
-  @published var speed: TimeInterval = 0.0
-  var item = PassThroughSubject<Void, Never>()
+  @Published var speed: TimeInterval = 0.0
+  var timeEvent = PassthroughSubject<Void, Never>()
+
   
   private var cancellable: AnyCancellable?
+  
+  init() {
+    cancellable = timeEvent
+      .measureInterval(using: RunLoop.main)
+      .sink { [unowned self] (stride) in
+        speed = stride.timeInterval
+      }
+  }
+  
 }
 
 struct MeasureInterval: View {
